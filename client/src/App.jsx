aa7b0e550fb4828c493b05789ce7dac85456fcf8
Dinuwarabandara5 <dinuwarabandara5@gmail.com>
2025-06-28 19:15:29 +0530

    routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
+import { Route } from 'react-router-dom'
+import LoginPage from './pages/LoginPage'
 
 const App = () => {
   return (
     <div>
-      
+      <Routes>
+        <Route path='\'element={<HomePage />} />
+        <Route path='\login'element={<LoginPage />} />
+        <Route path='\profile'element={<ProfilePage />} />
+
+       
+      </Routes>
     </div>
   )
 }
